fe3a21dd5e8418f605cc31e401763d9f3cd04759
sm1ke <smike143@yandex.ru>
2022-12-12 14:05:03 +0500

    feat: создать элементы для валидации форм

--- scripts/validate.js
+++ scripts/validate.js
@@ -15,8 +15,8 @@ const enableValidation = (config) => {
 enableValidation({
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_disabled',
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
 });
