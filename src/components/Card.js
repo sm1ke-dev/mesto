2b3775a895d8fdb7d280848e4c3463f9e8e0be07
sm1ke <smike143@yandex.ru>
2023-01-13 16:37:01 +0500

    feat: добавить правильную структуру

---
+++ src/components/Card.js
@@ -0,0 +1,46 @@
+export default class Card {
+  constructor(card, templateSelector, handleCardClick) {
+    this._name = card.name;
+    this._link = card.link;
+    this._templateSelector = templateSelector;
+    this._handleCardClick = handleCardClick;
+  }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._templateSelector)
+      .content
+      .querySelector('.element')
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  _setEventListeners() {
+    this._element.querySelector('.element__like-button').addEventListener('click', (evt) => {
+      this._handleLikeButton(evt);
+    });
+
+    this._element.querySelector('.element__trash-button').addEventListener('click', () => this._element.remove());
+
+    this._cardImage.addEventListener('click', () => this._handleCardClick(this._name, this._link));
+  }
+
+  _handleLikeButton(evt) {
+    evt.target.classList.toggle('element__like-button_is-active');
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+    this._cardImage = this._element.querySelector('.element__image');
+    this._cardTitle = this._element.querySelector('.element__name');
+
+    this._cardTitle.textContent = this._name;
+    this._cardImage.src = this._link;
+    this._cardImage.alt = this._name;
+
+    this._setEventListeners();
+
+    return this._element;
+  }
+}
